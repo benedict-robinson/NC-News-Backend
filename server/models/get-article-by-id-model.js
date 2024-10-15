04568aa04daeeee60b8bbf4a183457615ed24466
Benedict Robinson <benedict.r1713@gmail.com>
2024-10-15 09:17:22 +0100

    Changed get articles Id to prevent SQL injection

--- server/models/get-article-by-id-model.js
+++ server/models/get-article-by-id-model.js
@@ -1,8 +1,12 @@
 const db = require("../../db/connection.js")
 
 exports.selectArticleById = (id) => {
-    const selectByIdQuery = `SELECT article_id, title, topic, author, body, created_at::VARCHAR, votes, article_img_url FROM articles WHERE article_id = ${id};`
-    return db.query(selectByIdQuery)
+    const selectByIdQuery = `SELECT article_id, title, topic, author, body, created_at::VARCHAR, votes, article_img_url FROM articles WHERE article_id = $1;`
+    
+    if (!Number(id)) {
+        return Promise.reject({status: 400, msg: "Bad Request"})
+    }
+    return db.query(selectByIdQuery, [id])
     .then(({rows}) => {
         if (rows.length < 1) {
             return Promise.reject({status: 404, msg: "Article Not Found"})
